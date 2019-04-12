7ceaed6d2a51ba28e6f0240b437ace010fdc57f1
Marcell Csókási <csokasi.marcell@gmail.com>
2019-04-12 14:19:44 +0200

    deleting unnecessary components

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -8,14 +8,7 @@ const routes: Routes = [
         component: LayoutComponent,
         children: [
             { path: '', redirectTo: 'home', pathMatch: 'prefix' },
-            { path: 'home', loadChildren: './front-page/front-page.module#FrontPageModule' },
-            { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
-            { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
-            { path: 'forms', loadChildren: './form/form.module#FormModule' },
-            { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
-            { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
-            { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
-            { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
+            { path: 'home', loadChildren: './front-page/front-page.module#FrontPageModule' }
         ]
     }
 ];
